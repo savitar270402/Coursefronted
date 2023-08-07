2be252564805c52e89942e7a6ccba2d3ca1154cc
savitar270402 <138309404+savitar270402@users.noreply.github.com>
2023-08-07 20:52:50 +0600

    main

---
+++ LSN-1/lesson/main.js
@@ -0,0 +1,51 @@
+// console.timeLog('аааааааа');
+// alert('уведомление');
+
+// confirm('принять условия');
+// prompt('ваше имя');
+
+// var massage='принять условия';
+// confirm(massege);
+
+
+// let Firstname = 'принять условия';
+// const age = 99;
+// console.log(Firstname,age);
+
+// let isActive = confirm('авторизоваться');
+// console.log('isActive');
+
+let String = 'строка в ковычках'
+let number =1234569790
+let str = '12345678';
+let boolean1 = true;
+let boolean2 = false;
+
+let und = undefined;
+
+let n = null;
+let obj = {
+    name:'имя',
+    age:22,
+};
+let array =[1, 'str'];
+
+
+let sum = 2 + 2;
+console.log(sum);
+let difference = 5 - 3;
+let product = 4 * 6;
+
+let remainder = 15 % 4;
+console.log(remainder);
+
+let increment = 7;
+console.log(increment);
+increment++;
+console.log(increment);
+let decrement = 6;
+decrement --;
+
+let x = 10;
+x = x + 5;
+console.log(num);
